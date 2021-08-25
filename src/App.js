253e7d8a481b923a9fa265614c49457a6050b918
uktamyb <67410434+uktamyb@users.noreply.github.com>
2021-08-25 22:31:03 +0500

    Changes again I have done some changes!

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Uktam Hudoyarov</h1>
+      <h2>You can do any changes!</h2>
       <p>I am a React developer with 1 year of experience</p>
     </div>
   );
