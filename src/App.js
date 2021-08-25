27bce7fa85e5eb4abf5eec3b60651a979c4c7909
uktamyb <67410434+uktamyb@users.noreply.github.com>
2021-08-25 22:28:26 +0500

    Changes Changes have been done at 22:28

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <p>I am a React developer</p>
+      <p>I am a React developer with 1 year of experience</p>
     </div>
   );
 }
